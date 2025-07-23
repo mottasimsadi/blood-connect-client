b08f55498d4a900a629a2a0a06009d3202b6f8a9
mottasimsadi <mottasimsadi@gmail.com>
2025-07-24 02:54:03 +0600

    Project initialized with the necessary files

---
+++ src/Routers/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ErrorPage from "../pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+      {},
+    ],
+  },
+]);
+
+export default router;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router";
+import Header from "../components/Header";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <Header></Header>
+      <div className="flex min-h-screen items-center justify-center">
+        ErrorPage
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
